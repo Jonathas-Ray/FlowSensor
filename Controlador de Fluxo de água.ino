2440eea5280e4efe065b3595d351feaed88ed18d
Jonathas Ray <jonathasrmcguimaraes@gmail.com>
2024-11-22 01:35:04 -0300

    Adicionado um DIsplay
    Alterações para substituir os LEDs por um Display

--- Controlador de Fluxo de água.ino
+++ Controlador de Fluxo de água.ino
@@ -1,8 +1,11 @@
-#define MaxStorage 483        // 0,9817 L == 483.12 Tiques, onde cada 500 ML são 246;
+#include <LiquidCrystal.h>
+#define Tick = 500/246; // 0,9817 L == 483.12 Tiques, onde cada 500 ML são 246;
 
 int flowSensorInc = 2;  // Sensor 1
 int flowSensorDec = 3;  // Sensor 2
-int ledPins[] = {4, 5, 6, 7, 8, 9, 10};  //Facilita pra não ter que inicializar LED por LED em setup
+LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
+
+float totalStored_ml = 0.0;
 
 volatile int increasePulse = 0;  
 volatile int decreasePulse = 0; //Volatile porque vai ser manipulado fora do Loop pelo attachInterrupt
@@ -21,12 +24,14 @@ void setup() {
   pinMode(flowSensorInc, INPUT);  // Sensor 1
   pinMode(flowSensorDec, INPUT);  // Sensor 2
 
-  for (int i = 0; i < 7; i++) { //inicializa os LEDs
-    pinMode(ledPins[i], OUTPUT);
-  }
+  lcd.begin(16, 2);
 
   attachInterrupt(digitalPinToInterrupt(flowSensorInc), increaseStorage, RISING); // método para configurar interrupções contando os pulsos dos sensores
   attachInterrupt(digitalPinToInterrupt(flowSensorDec), decreaseStorage, RISING); // (entrada, função a ser executada, comportamento a ser observado no sensor)
+
+  lcd.print("Total armazenado:");
+  lcd.setCursor(0, 1);  // necessário p/ passar pra segunda linha, do contrário sobrescreve a primeira
+  lcd.print("%.3f Litros", totalStored_ml);
 }
 
 void loop() {
@@ -36,48 +41,5 @@ void loop() {
 
   totalStored -= decreasePulse;
   decreasePulse = 0;
-
-  controLEDs(totalStored);
-}
-
-void controLEDs(int totalStored) {
-    
-  float percentage = ((totalStored) / MaxStorage) * 100;
-
-  if (percentage = 0) {
-    turnTheLEDOn(0); // Primeiro LED vai piscar alertando que está zerado ou proximo de acabar
-  } else if (percentage <= 16.67) {
-    turnTheLEDOn(1); // 1/6, um LED aceso
-  } else if (percentage <= 33.33) {
-    turnTheLEDOn(2); // 2/6, dois LEDs acesos
-  } else if (percentage <= 50) {
-    turnTheLEDOn(3); // 3/6, três LEDs acesos
-  } else if (percentage <= 66.67) {
-    turnTheLEDOn(4); // 4/6, quatro LEDs acesos
-  } else if (percentage <= 83.33) {
-    turnTheLEDOn(5); // 5/6, cinco LEDs acesos
-  } else {
-    turnTheLEDOn(6); // Capacidade máxima, todos acesos
-  }
-} // int ledPins[] = {4(0), 5(1), 6(2), 7(3), 8(4), 9(5), 10(6)} 7 índices
-
-
-void turnTheLEDOn(int stored) { //acende os LEDs conforme a porcentagem do volume
-    if (stored == 0){
-        for(int i = 1; i <= 7; i++){
-            digitalWrite(ledPins[i], LOW);
-        }
-        digitalWrite(ledPins[0], HIGH);
-        delay(250);
-        digitalWrite(ledPins[0], LOW);
-        delay(250); //Atualizar +/- na velocidade que máxima de processamento de tiques pelo sensor para evitar perdas
-    } else {
-        for (int i = 1; i < 7; i++) {
-            if (i <= stored) {
-                digitalWrite(ledPins[i], HIGH);  // Acende o LED
-            } else {
-                digitalWrite(ledPins[i], LOW);   // Apaga o LED
-            }
-        }
-    }
+  totalStored_ml = totalStored * Tick;
 }
